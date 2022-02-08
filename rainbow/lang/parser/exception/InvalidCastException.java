da6a75ef1a06f1d8e5921f738534d79d48b717c9
Shivashish <dasshivashish11@gmail.com>
2022-02-09 01:26:19 +0530

    Added example for Cast statement and fixed error message for InvalidCastException

--- rainbow/lang/parser/exception/InvalidCastException.java
+++ rainbow/lang/parser/exception/InvalidCastException.java
@@ -4,6 +4,6 @@ import rainbow.lang.parser.Types;
 
 public class InvalidCastException extends ParserException {
     public InvalidCastException (Types toType) {
-        super("ERROR: cannot cast from string to " + toType.transform());
+        super("cannot cast from string to " + toType.transform());
     }
 }
